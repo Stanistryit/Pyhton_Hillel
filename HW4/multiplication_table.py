cf4965016ea3a0f0d760f0c2d0e86952d80aea85
Stanistryit <stanis.love.it@gmail.com>
2023-12-12 23:45:00 +0200

    HW 4

--- HW4/multiplication_table.py
+++ HW4/multiplication_table.py
@@ -1,8 +1,8 @@
 number = int(input("Введіть число від 1 до 100: "))
 
-while 0 == number <= 101:
+while number < 1 or number > 100:
   print("Число має бути в діапазоні від 1 до 100.")
   number = int(input("Введіть число від 1 до 100: "))
 
-for i in range(1, 11):
+for i in range(1, 10):
   print(f"{number} * {i} = {number * i}")
