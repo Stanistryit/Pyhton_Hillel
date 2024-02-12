8bdf9ace106e11ccacbddc24aedd6ef5d910c631
Stanistryit <109979964+Stanistryit@users.noreply.github.com>
2024-02-12 10:51:44 +0200

    HW14 update

--- HW14/O(size).py
+++ HW14/O(size).py
@@ -24,14 +24,24 @@ assert eye_matrix(4) == [[1, 0, 0, 0],
 
 
 def eye_matrix_v2(size):
-    # Створює матрицю з 0 розміром `size` x `size` і заповнює діагональ одиницями
-    matrix = [[1 if x == y else 0 for y in range(size)] for x in range(size)]
-    return matrix
+    # Використання генератора для створення рядків матриці
+    for x in range(size):
+        yield [1 if x == y else 0 for y in range(size)]
 
-assert eye_matrix_v2(3) == [[1, 0, 0],
-                           [0, 1, 0],
-                           [0, 0, 1]]
-assert eye_matrix_v2(4) == [[1, 0, 0, 0],
-                           [0, 1, 0, 0],
-                           [0, 0, 1, 0],
-                           [0, 0, 0, 1]]
+
+def test_eye_matrix_v2(size, expected_matrix):
+    gen = eye_matrix_v2(size)
+    for expected_row in expected_matrix:
+        assert next(gen) == expected_row
+    try:
+        next(gen)
+        assert False
+    except StopIteration:
+        pass
+
+# Тестування функції
+test_eye_matrix_v2(3, [[1, 0, 0], [0, 1, 0], [0, 0, 1]])
+test_eye_matrix_v2(4, [[1, 0, 0, 0], [0, 1, 0, 0], [0, 0, 1, 0], [0, 0, 0, 1]])
+
+
+
